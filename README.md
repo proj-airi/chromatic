a3ab150cefbf5b0d562c38da34d59cc2c823bded
Makito <i@maki.to>
2025-06-19 05:04:19 +0900

    feat: preset and playground
    Co-authored-by: Makito <5277268+sumimakito@users.noreply.github.com>
    Co-authored-by: RainbowBird <44741987+luoling8192@users.noreply.github.com>
    Co-authored-by: LemonNeko <17664845+LemonNekoGH@users.noreply.github.com>
    Co-authored-by: junkwarrior87 <115852752+junkwarrior87@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# `mahohue`
+# `chromatic`
 
 ### Development
 
---
+++ playground/uno.config.ts
@@ -0,0 +1,26 @@
+import { presetChromatic } from '@proj-airi/preset-chromatic'
+import { defineConfig, presetMini } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetMini(),
+    presetChromatic({
+      baseHue: 300,
+      colors: {
+        zero: 0,
+        thirty: 30,
+        sixty: 60,
+        ninety: 90,
+        hundredTwenty: 120,
+        hundredFifty: 150,
+        hundredEighty: 180,
+        twoTen: 210,
+        twoForty: 240,
+        twoSeventy: 270,
+        threeHundred: 300,
+        threeThirty: 330,
+      },
+      bakeColors: false,
+    }),
+  ],
+})
