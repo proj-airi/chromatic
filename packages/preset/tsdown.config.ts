6c1f0afecd399bcdcba67255ff9ca89b18344e76
Neko <neko@ayaka.moe>
2025-06-19 16:13:56 +0800

    feat(preset): use conditional exports to prevent import of `node:process` fail on client and REPL (#3)
    Co-authored-by: Makito <i@maki.to>

--- packages/preset/tsdown.config.ts
+++ packages/preset/tsdown.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
+  entry: {
+    'index': './src/index.ts',
+    'index.node': './src/index.node.ts',
+  },
   dts: true,
-  exports: true,
-  entry: ['./src'],
+  unused: true,
+  fixedExtension: true,
 })
